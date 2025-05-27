aac18e7e83a275a494d5f132f2fa9f0ab40bc249
Leti <leticia.figueroa.r07@gmail.com>
2025-05-27 13:15:50 -0300

    feat(GlobalControllerExceptionHandler): now, in case of a incomplet reuest, the handler doesnt returns a text. returns a response with all the fileds not received. still returning with error 400. Drop dummy version

--- backend/src/main/java/ar/uba/fi/ingsoft1/todo_template/config/GlobalControllerExceptionHandler.java
+++ backend/src/main/java/ar/uba/fi/ingsoft1/todo_template/config/GlobalControllerExceptionHandler.java
@@ -4,9 +4,14 @@ import ar.uba.fi.ingsoft1.todo_template.common.exception.ItemNotFoundException;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,28 +19,21 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalControllerExceptionHandler {
 
-    @ExceptionHandler(value = MethodArgumentNotValidException.class, produces = "text/plain")
-    @ApiResponse(
-            responseCode = "400",
-            description = "Invalid arguments supplied",
-            content = @Content(
-                    mediaType = "text/plain",
-                    schema = @Schema(implementation = String.class, example = "Validation failed because x, y, z")
-            )
-    )
-    public ResponseEntity<String> handleMethodArgumentInvalid(MethodArgumentNotValidException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(value = ItemNotFoundException.class, produces = "text/plain")
-    @ApiResponse(
-            responseCode = "404",
-            description = "Referenced entity not found",
-            content = @Content(
-                    mediaType = "text/plain",
-                    schema = @Schema(implementation = String.class, example = "Failed to find foo with id 42")
-            )
-    )
+    @ApiResponse(responseCode = "404", description = "Referenced entity not found", content = @Content(mediaType = "text/plain", schema = @Schema(implementation = String.class, example = "Failed to find foo with id 42")))
     public ResponseEntity<String> handleItemNotFound(ItemNotFoundException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
@@ -50,7 +48,6 @@ public class GlobalControllerExceptionHandler {
     public ResponseEntity<String> handleFallback(Throwable ex) {
         return new ResponseEntity<>(
                 ex.getClass().getCanonicalName() + " " + ex.getMessage(),
-                HttpStatus.INTERNAL_SERVER_ERROR
-        );
+                HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
