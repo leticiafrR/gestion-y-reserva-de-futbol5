8fdb46d5437a00c6e89847976929177b2f626707
Leti <leticia.figueroa.r07@gmail.com>
2025-05-30 17:19:49 -0300

    feat(UseCreateDTO): adding messages in case of emtys fiels in a "register user" request

--- backend/src/main/java/ar/uba/fi/ingsoft1/todo_template/user/UserCreateDTO.java
+++ backend/src/main/java/ar/uba/fi/ingsoft1/todo_template/user/UserCreateDTO.java
@@ -5,15 +5,23 @@ import jakarta.validation.constraints.Email;
 
 import java.util.function.Function;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record UserCreateDTO(
-        @NotBlank String username,
-        @NotBlank String password,
-        @NotBlank @Email String email,
-        String gender,
-        String age,
-        String address
-) implements UserCredentials {
+        @Schema(description = "Username address") @NotBlank(message = "The username cannot be empty to register a user.") String username,
+        @Schema(description = "User's email address") @NotBlank(message = "The user email cannot be empty to register a user.") @Email String email,
+        @Schema(description = "User's password") @NotBlank(message = "The user's password cannot be empty to register a user.") String password,
+        @Schema(description = "User's role") @NotBlank(message = "The user role cannot be empty to register a user.") String role,
+        @Schema(description = "User's gender") @NotBlank(message = "The user gender cannot be empty to register a user.") String gender,
+        @Schema(description = "User's age") @NotBlank(message = "The user age cannot be empty to register a user.") String age,
+        @Schema(description = "User's address") @NotBlank(message = "The user address cannot be empty to register a user.") String address)
+
+        implements UserCredentials {
+
     public User asUser(Function<String, String> encryptPassword) {
-        return new User(username, encryptPassword.apply(password), email, gender, age, address);
+        return new User(username, email,
+                encryptPassword.apply(password), role, gender,
+                age, address);
     }
+
 }
