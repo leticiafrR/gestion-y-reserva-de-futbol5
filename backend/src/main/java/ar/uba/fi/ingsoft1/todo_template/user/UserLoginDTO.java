db9cf472fee32d4c65ba0aa16c44762a60fce64a
Leti <leticia.figueroa.r07@gmail.com>
2025-05-31 16:43:56 -0300

    style(UserLoginDto): delete not used import

--- backend/src/main/java/ar/uba/fi/ingsoft1/todo_template/user/UserLoginDTO.java
+++ backend/src/main/java/ar/uba/fi/ingsoft1/todo_template/user/UserLoginDTO.java
@@ -1,10 +1,9 @@
 package ar.uba.fi.ingsoft1.todo_template.user;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record UserLoginDTO(
-        @NotBlank @Email String username,
-        @NotBlank String password
-) implements UserCredentials {}
+                @NotBlank @Email String username,
+                @NotBlank String password) implements UserCredentials {
+}
